a784fcaba0889b2a4b114caa5eb920b3b116f827
amisha2016 <amishabudhiraja96@gmail.com>
2017-07-07 22:00:31 +0530

    changes

--- getxml.py
+++ getxml.py
@@ -2,10 +2,10 @@
 import fileinput
 import os
 import urllib
-#fullfilename = os.path.join('/home/amisha/python_programs/style.xml')
-#print fullfilename
-#urllib.urlretrieve("https://raw.githubusercontent.com/GreatDevelopers/pbOSM/master/style.xml",fullfilename)
+fullfilename = os.path.join('/home/amisha/python_programs/style.xml')
+print fullfilename
+urllib.urlretrieve("https://raw.githubusercontent.com/GreatDevelopers/pbOSM/master/style.xml",fullfilename)
 
-for line in fileinput.FileInput("/home/amisha/osm/openstreetmap-carto/bound.xml",inplace=1):
+for line in fileinput.FileInput("style.xml",inplace=1):
     line = line.replace("[gis]","[india]")
     print line
